34c674fc64d7eaeac4cd4ca92aadcfc57befcf2a
Daksh Shah <daksh17336@iiitd.ac.in>
2019-01-28 19:12:40 +0530

    Finalizing

--- hw5.c
+++ hw5.c
@@ -1,96 +1,71 @@
 #include <stdio.h>
 #include <unistd.h>
 //ls | wc -l
-/*
+
+/* File Descriptors Mapping
 0 - STDIN
 1 - STDOUT
-2 - STDERR
+2 - STDERR */
 
-You need to use close
-and dup system calls to redirect the STDOUT of ls to the STDIN of wc
-
-dup -> https://www.geeksforgeeks.org/dup-dup2-linux-system-call/
-
-The dup() system call creates a copy of a file descriptor.
-
-It uses the lowest-numbered unused descriptor for the new descriptor.
-If the copy is successfully created, then the original and copy file descriptors may be used interchangeably.
-They both refer to the same open file description and thus share file offset and file status flags.
-int dup(int oldfd);
-oldfd: old file descriptor whose copy is to be created.
-
-
-
-close -> http://pubs.opengroup.org/onlinepubs/009696899/functions/close.html
-*/
 int main(){
 	int pid; //Process ID
-	int fd[2]; //File Descriptor, to store the ends of pipe I believe
-	/* 0 - Read End
-	   1 - Write End*/
-
-	int ret; //Return value ?
-
-	// fd currently has garbage values
-	// printf("fd[] = %d, %d\n", fd[0],fd[1]);
-	// fd[] = -308746176, 32766
 	
-	/* Cannot find much about this function from the man page and Clion.
-	From what I understand it just tries to create a pipe
+	int fd[2]; //(File Descriptors) To store the ends of the pipe, in future
+	/* fd[0] - Read End
+	   fd[1] - Write End*/
 
-	it also sets the values in fd
-	returns -1 when pipe cannot be created*/
+	int ret; //Return value
+
+	/* Creates a pipe, and sets the values of the ends in fd variable
+	returns -1 :  when pipe cannot be created
+			0  :  when pipe is created successfully*/
 	ret = pipe (fd); 
 	if (ret == -1) {
 		printf ("Unable to create pipe\n");
 		return 0;
 	}
-	// printf("pipe(fd) returned %d\n", ret);
-	// returned 0
-
-	// printf("fd[] = %d, %d\n", fd[0],fd[1]);
-	// fd[] = 3, 4
 
 	pid = fork ();
-	//Trying to create a child process, 
-	//so the execution splits/forks here and two instances 
-	//start executing from the very next line
-
-	if (pid == 0) { //CHILD, writes to the pipe
+	/* Tries to fork, i.e. to create a child process
+	Two parallel executions (parent, child) start from the next line
+	*/
+	
+	if (pid == 0) { //Child; executes ls; writes to the pipe
 		/* Verify that ls exists at /bin/ls */
 		/* to verify run: which ls */
 		char* const args[] = {"/bin/ls", NULL};
-		/* write your code here 
-		Close STDOUT*/
-		close(fd[0]);//Closing read end of the pipe
-		//non-essential, good coding, so that fd table entry can be used for other purposes
-
-		close(1);//Closing STDOUT
-		dup(fd[1]);//Duplicating Write end of pipe (to 1)
 		
-		close(fd[1]);//good coding, so that fd table entry can be used for other purposes
-		//Closing write end of the pipe
+		close(fd[0]); //[^1] Closing the read end of the pipe 
+		
+		close(1);//Closing STDOUT
+		dup(fd[1]);//Duplicating Write End of pipe (to 1 - which is the lowest FD available)
+
+		close(fd[1]); //[^1] Closing write end of the pipe
 
 		ret = execv (args[0], args);
+		
 		/* NOT REACHED*/
 		printf ("failed to exec ls\n");
-	} else if (pid > 0) { //Parent reads from the pipe
+	} else if (pid > 0) { //Parent; executes wc; reads from the pipe
 		/* Verify that wc exists at /usr/bin/wc */
 		/* to verify run: which wc */
 		char* const args[] = {"/usr/bin/wc", "-l", NULL};
-		/* write your code here 
-		Close STDIN*/
-		close(fd[1]);//ESSENTIAL, OTHERWISE CODE WONT STOP
-		//Closing write end of the pipe
 		
+		close(fd[1]); //Closing write end of the pipe
+		/* Essential, Otherwise no output will come and it will be hung in wait for more input
+				Needed because, while reading from the pipe (done in wc), 
+				it waits till it can get some input;
+				i.e. Any threads have the write end of pipe open
+
+				PS: after receiving EOF, it considers that particular thread's write end to be closed
+		*/
+
 		close(0); //Closing STDIN
-		dup(fd[0]);//Duplicating Read end of pipe (to 0)
+		dup(fd[0]);//Duplicating Read end of pipe (to 0 - which is the lowest FD available)
 		
-		close(fd[0]);//good coding, so that fd table entry can be used for other purposes
-		//Closing read end of the pipe
+		close(fd[0]); //[^1] Closing read end of the pipe
 
 		execv (args[0], args);
-		// close(fd[0]);//no point of this
 		
 		/* NOT REACHED*/
 		printf ("failed to exec wc\n");
@@ -98,4 +73,10 @@ int main(){
 		printf ("Unable to fork\n");
 	}
 	return 0;
+
+	/*
+	[^1] : non-essential for the right output; done as a good coding practice
+	so that fd table entry can be used for other purposes
+
+	*/
 }
