c3f4f4353fe21b84f006b61d63baf65d75f84976
Daksh Shah <daksh17336@iiitd.ac.in>
2019-03-12 13:28:43 +0530

    Used mmap now

--- Homework 10/source.c
+++ Homework 10/source.c
@@ -5,6 +5,7 @@
 #include <unistd.h> 
 #include <stdlib.h> 
 #include <string.h> 
+#include <sys/mman.h>
 
 #define NUM_PAGES 1024
 #define PAGE_SIZE 4096
@@ -21,18 +22,12 @@ void write_file(int fd, char * buf) {
     }
 }
 
-void read_file(int fd, char * buf) {
+void read_file(void * mappedMemory, char * buf) {
     int i, ret;
     char str[PAGE_SIZE];
-    lseek(fd, 0, SEEK_SET);
 
     for (i = 0; i < NUM_PAGES; i++) {
-        ret = read(fd, str, PAGE_SIZE);
-        if (ret != PAGE_SIZE) {
-            printf("unable to read %d\n", ret);
-            exit(0);
-        }
-        if (memcmp(str, buf, PAGE_SIZE) != 0) {
+        if (memcmp(mappedMemory, buf, PAGE_SIZE) != 0) {
             printf("data don’t match\n");
             exit(0);
         }
@@ -42,10 +37,9 @@ void read_file(int fd, char * buf) {
 int main() {
     int i, fd, ret;
     char buf[PAGE_SIZE];
-    
-    for (i = 0; i < PAGE_SIZE; i++) {
+
+    for (i = 0; i < PAGE_SIZE; i++)
         buf[i] = rand() % 128;
-    }
     fd = creat("/tmp/temp.txt", S_IRUSR | S_IWUSR);
     if (fd < 0) {
         printf("error in creat\n");
@@ -53,10 +47,21 @@ int main() {
     }
     write_file(fd, buf);
     close(fd);
+    
     fd = open("/tmp/temp.txt", O_RDONLY);
-    for (i = 0; i < 1024; i++) {
-        read_file(fd, buf);
-    }
+    
+    //Mapping the File to Memory
+    //     mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+    void * mappedMemory = mmap(NULL, NUM_PAGES*PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
+    if(mappedMemory == (void *) -1)
+        exit(1);
+
+    for (i = 0; i < 1024; i++)
+        read_file(mappedMemory, buf);
+
+    //unmap: Releasing the memory
+    munmap (mappedMemory, NUM_PAGES*PAGE_SIZE);
+
     close(fd);
     return 0;
 }
